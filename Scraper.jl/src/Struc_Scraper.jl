ace8a45abf10f868a0a23525f01af4834b1a4d4d
Eve <evecheng999@gmail.com>
2024-02-13 16:26:12 +1100

    EzXML

---
+++ Scraper.jl/src/Struc_Scraper.jl
@@ -0,0 +1,49 @@
+using LightXML
+
+function test()
+    xdoc = parse_file("urls/xml_files/2019-02-12.xml")
+    xroot = root(xdoc)
+    for c in child_elements(xroot)
+        if is_elementnode(c)
+            for ele in get_elements_by_tagname(c, "answers.to.questions") 
+                @show content(ele)
+            end
+            if name(c) == "answers.to.questions"
+                debates = collect(child_elements(c))
+                for debate in debates
+                    for subdebates in child_elements(debate)
+                        if name(subdebates)=="debate.text"
+                            @show content(subdebates)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+function write_child_nodes_recursive(root,file,depth,indent_level::Int = 0)
+    indent = repeat("  ", indent_level)    
+    if depth <= 0
+        return
+    end
+    
+    for child in child_nodes(root)
+        if is_elementnode(child)
+            write(file, "$indent $(name(child)) \n")
+            print("$indent $(name(child)) \n")
+            write_child_nodes_recursive(child, file,depth - 1, indent_level + 1)
+        end
+    end
+end
+
+function write_main()
+    xdoc = parse_file("urls/xml_files/2019-02-12.xml")
+    xroot = root(xdoc)
+    open("child_nodes.txt", "w") do file
+        write_child_nodes_recursive(xroot,file,3,0)
+    end
+end
+ 
+
--- Scraper.jl/src/XML_Scraper.jl
+++ Scraper.jl/src/XML_Scraper.jl
@@ -1,41 +1,16 @@
-using LightXML
+using EzXML
 
 function test()
-    xdoc = parse_file("urls/xml_files/2019-02-12.xml")
-    xroot = root(xdoc)
-    for c in child_nodes(xroot)
-        if is_elementnode(c)
-            @show name(c)
-        end
-        for i in child_nodes(c)
-            if is_elementnode(i)
-                @show name(i)
-            end
-        end    
+    xdoc = readxml("urls/xml_files/2019-09-09.xml")
+    primates = root(xdoc)
+    for child in eachelement(primates)
+        @show typeof(child)
+        @show child.content
+    end
+    # Find texts using XPath query.
+    for species_name in nodecontent.(findall("//debate", primates))
+#        print("AGGGGGGGGGGGG")
+#        println("- ", species_name)
     end
 end
-
-function write_child_nodes_recursive(root,file,depth,indent_level::Int = 0)
-    indent = repeat("  ", indent_level)    
-    if depth <= 0
-        return
-    end
-    
-    for child in child_nodes(root)
-        if is_elementnode(child)
-            write(file, "$indent $(name(child)) \n")
-            print("$indent $(name(child)) \n")
-            write_child_nodes_recursive(child, file,depth - 1, indent_level + 1)
-        end
-    end
-end
-
-function write_main()
-    xdoc = parse_file("urls/xml_files/2019-02-12.xml")
-    xroot = root(xdoc)
-    open("child_nodes.txt", "w") do file
-        write_child_nodes_recursive(xroot,file,3,0)
-    end
-end
- 
 
