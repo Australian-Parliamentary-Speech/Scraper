eee394e5b22d413de80569bcf5861478b494f5be
Eve <evecheng999@gmail.com>
2024-03-24 15:52:52 +1100

    -a

--- Scraper.jl/src/XML_Scraper.jl
+++ Scraper.jl/src/XML_Scraper.jl
@@ -2,26 +2,8 @@ union!(LOAD_PATH, ["/home/eve/Desktop/Parlinfo_text_project/Scraper.jl/src"])
 using EzXML
 using utils
 using Questions
+using CSV
 
-#next step: resume ordering by abandoning dicts
-function test()
-#    xdoc = readxml("urls/test_files/2023-12-07.xml")
-    xdoc = readxml("urls/test_files/check.xml")
-    soup = root(xdoc)
-    q_dict,a_dict = question_time(soup)
-#    q_dict,a_dict = answer_to_questions(soup)
-    q_to_a = scrape_question_time(q_dict,a_dict)
-    for key in keys(q_to_a)
-        @show filter_(q_to_a[key][1].content)
-        try
-            @show filter_(q_to_a[key][2].content)
-        catch
-            print("Nothing?")
-            @show q_to_a[key][2]
-        end
-    end
-    return soup
-end
 
 function talker_from_qa(path,soup)
     talker_nodes = findall("$(path)//talker",soup)
@@ -33,9 +15,34 @@ function talker_from_qa(path,soup)
     end
 end
 
+function read_csv_columns(filename::AbstractString, column_indice::Int)
+    df = CSV.File(filename) |> DataFrame
+    column = df[:, i]
+    return column
+end
+
+function test()
+    xdoc = readxml("urls/test_files/2023-12-07.xml")
+    soup = root(xdoc)
+    q_dict,a_dict=question_time(soup)
+    q_to_a = scrape_question_time(q_dict,a_dict)
+    fn = "question_to_answers_.csv"
+    open(fn, "w") do io
+        for key in keys(q_to_a)
+            question = filter_(q_to_a[key][1][1].content)[1:100]
+            answers = [filter_(i.content)[1:100] for i in q_to_a[key][2]]
+            row = [question, answers...]
+            edit_row = ' '
+            for i in row
+                edit_row = edit_row * "\"$i\","
+            end
+            @show edit_row
+            println(io,edit_row)
+        end
+    end
+end 
 
 function test2()
-#    xdoc = readxml("urls/test_files/2023-12-07.xml")
     xdoc = readxml("urls/test_files/check.xml")
     soup = root(xdoc)
 #    inter = find_node(soup,"chamber.xscript//interjection")
