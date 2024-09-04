b7972e13198a779c441a086308d8b71a768df5dd
Eve <evecheng999@gmail.com>
2024-09-04 15:51:53 +1000

    more testing on csv

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,26 @@ using Test
 
 const RunModule = ParlinfoSpeechScraper.RunModule
 
-@testset verbose = true "AbstractPhase" begin
-    @test begin
+function check_csv(curr,correct)
+    file_curr = open(curr, "r") do f
+        readlines(f)
+    end
+
+    file_correct = open(correct, "r") do f
+        readlines(f)
+    end
+    return file_curr == file_correct
+end 
+
+#this set of test looks at before edit only.
+@testset verbose = false "AbstractPhase" begin
+    @test  begin
         files = readdir(joinpath(@__DIR__,"xmls/AbstractPhase/"))
-        output_path = joinpath(dirname(@__FILE__),"result/AbstractPhase")
+        output_path = joinpath(dirname(@__FILE__),"step1_result/AbstractPhase")
         for file in files
-            date = RunModule.run_xml(joinpath(@__DIR__,"xmls/AbstractPhase/$file"),output_path,false,true)
-            mv(joinpath(output_path,"$(date)_edit_step1.csv"),joinpath(output_path,"$(date)_edit_step1_$(file[1:end-4]).csv"),force=true)
-            mv(joinpath(output_path,"$(date)_edit_step2.csv"),joinpath(output_path,"$(date)_edit_step2_$(file[1:end-4]).csv"),force=true)
+            date = RunModule.run_xml(joinpath(@__DIR__,"xmls/AbstractPhase/$file"),output_path,false,false)
+#            mv(joinpath(output_path,"$(date)_edit_step1.csv"),joinpath(output_path,"$(date)_edit_step1_$(file[1:end-4]).csv"),force=true)
+#            mv(joinpath(output_path,"$(date)_edit_step2.csv"),joinpath(output_path,"$(date)_edit_step2_$(file[1:end-4]).csv"),force=true)
             mv(joinpath(output_path,"$(date).csv"),joinpath(output_path,"$(date)_$(file[1:end-4]).csv"),force=true)
         end
         
@@ -21,21 +33,36 @@ const RunModule = ParlinfoSpeechScraper.RunModule
         for file in current_files
             curr = joinpath(output_path,file)
             correct = joinpath("$(output_path)/correct/",file)
-            file_curr = open(curr, "r") do f
-                readlines(f)
-            end
-
-            file_correct = open(correct, "r") do f
-                readlines(f)
-            end
-            if file_curr != file_correct
-                @info file_curr
+            if !check_csv(curr,correct)
                 return false
             end
-       end
-    true
+      end
+    true 
     end
-
 end
 
+@testset verbose = true "Edit" begin
+    @test begin
+        PhaseType = AbstractPhase
+        test_path = joinpath(@__DIR__,"csvs/AbstractPhase")
+        test_files = readdir(test_path)
+        test_csvs = filter(f -> endswith(f, ".csv"), test_files)
+        for test_csv in test_csvs
+            RunModule.EditModule.edit_csv(joinpath(test_path,test_csv),PhaseType)
+        end
+        result_files = readdir(test_path)
+        result_csvs = filter(f -> occursin("step", f), result_files)
+        for result_csv in result_csvs
+            mv(joinpath(test_path,result_csv),joinpath(@__DIR__,"step2_result/AbstractPhase/$result_csv"),force=true)
+        end
+        for result_csv in result_csvs
+            curr = joinpath(@__DIR__,"step2_result/AbstractPhase/$result_csv")
+            correct = joinpath(@__DIR__,"step2_result/AbstractPhase/correct/$result_csv")
+            if !check_csv(curr,correct)
+                return false
+            end
+        end
+       true         
+    end
+end
 
