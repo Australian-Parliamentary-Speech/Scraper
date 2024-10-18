337e0f5013fd8871bc5c7192f3e481ec36539e10
Eve <evecheng999@gmail.com>
2024-10-18 16:39:13 +1100

    tests working

--- src/nodes/Phases/Phase2011/Phase2011.jl
+++ src/nodes/Phases/Phase2011/Phase2011.jl
@@ -21,11 +21,11 @@ define_flags(node::Node{<:AbstractNode{Phase2011}},parent_node,node_tree)
 Added petition and quote nodes on top of the previous flags
 """
 function define_flags(node::Node{<:AbstractNode{Phase2011}},parent_node,node_tree)
-    ParentTypes = [QuestionNode,AnswerNode,InterjectionNode,SpeechNode,PetitionNode,QuoteNode_{Phase2011}]
+    ParentTypes = [QuestionNode,AnswerNode,InterjectionNode,SpeechNode,PetitionNode,QuoteNode_{Phase2011},MotionnospeechNode]
 #    if parent_node isa Node{QuoteNode_{Phase2011}} && !(node_tree[end-1] isa Node{DebateNode{Phase2011}})
 #        parent_node = node_tree[end-1]
 #    end
-    headers = ["question_flag","answer_flag","interjection_flag","speech_flag","petition_flag","quote_flag"]
+    headers = ["question_flag","answer_flag","interjection_flag","speech_flag","petition_flag","quote_flag","motionnospeech_flag"]
     flags = map(node_type -> parent_node isa Node{<:node_type} ? 1 : 0, ParentTypes)
     header_and_flag = zip(headers,flags)
     for couple in header_and_flag
@@ -36,7 +36,7 @@ end
 
 
 function define_headers(::Type{Phase2011})
-    headers = ["question_flag","answer_flag","interjection_flag","speech_flag","petition_flag","quote_flag","chamber_flag","name","name.id","electorate","party","role","page.no","content","subdebateinfo","debateinfo","path"]
+    headers = ["question_flag","answer_flag","interjection_flag","speech_flag","petition_flag","quote_flag","motionnospeech_flag","chamber_flag","name","name.id","electorate","party","role","page.no","content","subdebateinfo","debateinfo","path"]
     headers_dict = OrderedDict(headers .=> ["N/A" for h in headers])
     return headers_dict
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,14 @@ using CSV
 
 const RunModule = ParlinfoSpeechScraper.RunModule
 
+
+function create_dir(directory_path::String)
+    if !isdir(directory_path)
+        mkpath(directory_path)
+    end
+end
+
+
 function check_csv(curr,correct)
     file_curr = open(curr, "r") do f
         readlines(f)
@@ -19,31 +27,31 @@ end
 
 
 #this set of test looks at before edit only.
-@testset verbose = false "AbstractPhase" begin
+@testset verbose = false "Step1 and Step2" begin
     @test  begin
-        files = readdir(joinpath(@__DIR__,"xmls/AbstractPhase/"))
-        output_path = joinpath(dirname(@__FILE__),"step1_result/AbstractPhase")
-        for file in files
-            date = RunModule.run_xml(joinpath(@__DIR__,"xmls/AbstractPhase/$file"),output_path,false,false)
-#            mv(joinpath(output_path,"$(date)_edit_step1.csv"),joinpath(output_path,"$(date)_edit_step1_$(file[1:end-4]).csv"),force=true)
-#            mv(joinpath(output_path,"$(date)_edit_step2.csv"),joinpath(output_path,"$(date)_edit_step2_$(file[1:end-4]).csv"),force=true)
-            mv(joinpath(output_path,"$(date).csv"),joinpath(output_path,"$(date)_$(file[1:end-4]).csv"),force=true)
-        end
-        
-        current_files = readdir(output_path)
-        current_csvs = filter(f -> endswith(f, ".csv"), current_files)
-        for file in current_files
-            curr = joinpath(output_path,file)
-            correct = joinpath("$(output_path)/correct/",file)
-            if !check_csv(curr,correct)
-                return false
+        pass = true
+        for Phase in ["AbstractPhase","Phase2011"]
+            files = filter(!isdir,readdir(joinpath(@__DIR__,"xmls/$(Phase)/")))
+            output_path = joinpath(dirname(@__FILE__),"step1_result/$(Phase)")
+            create_dir(output_path)
+            for file in files
+                date = RunModule.run_xml(joinpath(@__DIR__,"xmls/$(Phase)/$file"),output_path,false,false)
+                mv(joinpath(output_path,"$(date).csv"),joinpath(output_path,"$(date)_$(file[1:end-4]).csv"),force=true)
             end
-      end
-    true 
-    end
-end
 
-@testset verbose = true "Edit" begin
+            current_files = filter(f -> endswith(f,".csv"),readdir(output_path))
+            current_csvs = filter(f -> endswith(f, ".csv"), current_files)
+            for file in current_files
+                curr = joinpath(output_path,file)
+                correct = joinpath("$(output_path)/correct/",file)
+                if !check_csv(curr,correct)
+                    pass = false
+                end
+            end
+        end
+        pass
+    end
+
     @test begin
         PhaseType = AbstractPhase
         test_path = joinpath(@__DIR__,"csvs/AbstractPhase")
@@ -64,7 +72,7 @@ end
                 return false
             end
         end
-       true         
+       return true         
     end
 end
 
