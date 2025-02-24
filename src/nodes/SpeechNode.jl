4efde62a6e168a24818a1941313d33c782e767c8
Eve <evecheng999@gmail.com>
2025-02-24 19:09:28 +1100

    answer node fixed

--- src/nodes/SpeechNode.jl
+++ src/nodes/SpeechNode.jl
@@ -8,23 +8,11 @@ function is_nodetype(node, node_tree,nodetype::Type{<:SpeechNode},phase::Type{<:
     nodetype = nodetype{phase}
     allowed_names = get_xpaths(nodetype)
     name = nodename(node)
-    try
-        title = find_section_title(node_tree,soup,DebateNode{phase})
-        if title != "QUESTIONS WITHOUT NOTICE"
-            if name == "motionnospeech"
-            end
-            return name in allowed_names
-        else 
-            return false
-        end
-    catch e
-        @error e
-        return false
-    end
+    return name in allowed_names
 end
 
 function get_xpaths(::Type{<:SpeechNode})
-    return ["speech","question","answer","continue"]
+    return ["speech","continue"]
 end
 
 
