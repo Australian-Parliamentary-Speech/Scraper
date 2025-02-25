734cd17f41f0b1816fe873ed76d120ec10bafd17
Eve <evecheng999@gmail.com>
2025-02-25 17:19:34 +1100

    flatten issue

--- src/edit_funcs/flatten.jl
+++ src/edit_funcs/flatten.jl
@@ -68,36 +68,53 @@ function get_row(rows, row_no)
     return row_[1]
 end
 
+function equiv(current_row,next_row,header_to_num)
+    function change_flag(flags)
+        if flags[header_to_num[:quote_flag]] == 1
+            flags[header_to_num[:quote_flag]] = 0
+            flags[header_to_num[:speech_flag]] = 1
+            return flags
+        end
+        return flags
+    end
+ 
+    flag_indices, current_flags = find_all_flags(current_row,header_to_num)
+    flag_indices, next_flags = find_all_flags(next_row,header_to_num)
+    current_flags = change_flag(current_flags)
+    next_flags = change_flag(next_flags)
+    return current_flags == next_flags
+end
 
 function stop_before_next_talker(row_no,rows,header_to_num,log)
     if is_stage_direction(rows[row_no],header_to_num)
         return true
     else
         next_row = get_row(rows,row_no)
-        name_pos = header_to_num[:name]
 
+        name_pos = header_to_num[:name]
         if next_row[name_pos] != "N/A"
             return true
         else
-            current_row = get_row(rows,row_no - 1)
-            current_flags = find_all_flags(current_row,header_to_num)
-            next_flags = find_all_flags(next_row,header_to_num)
-            return !(current_flags == next_flags)
+           current_row = get_row(rows,row_no - 1)
+           return !(equiv(current_row,next_row,header_to_num))
         end 
     end
     return false
 end
 
+
 """get all flags except chamber flag"""
 function find_all_flags(row,header_to_num)
-    all_flags = [process_flag(row[header_to_num[k]]) for k in keys(header_to_num) if (occursin("flag",string(k)) && !(occursin("chamber",string(k))))]
-    return all_flags
+ #   all_flags = [process_flag(row[header_to_num[k]]) for k in keys(header_to_num) if (occursin("flag",string(k)) && !(occursin("chamber",string(k))))]
+    flag_indices = sort([header_to_num[k] for k in keys(header_to_num) if (occursin("flag",string(k)) && !(occursin("chamber",string(k))))])
+    all_flags = [process_flag(row[f]) for f in flag_indices]
+    return flag_indices,all_flags
 end 
 
 function is_stage_direction(row,header_to_num)
     row_ = @. collect(row)
     row = row_[1]
-    all_flags = find_all_flags(row,header_to_num)
+    flag_indices, all_flags = find_all_flags(row,header_to_num)
     if all(==(0), all_flags)
         return true
     end  
